f6a1964e2237588b0da13f8d8fe36e17830569a6
Bulcao <ambulcao@gmail.com>
2020-09-20 23:27:59 +0100

    Create function Base Pizza

--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Cheese from "../assets/BaseCheese.png";
+import Base from "../assets/PizzaBase.png";
+import Olive from "../assets/Olive.png";
+import Pineapple from "../assets/Pineapple.png";
+import Mushroom from "../assets/Mushroom.png";
+import Basil from "../assets/Basil.png";
+import Tomato from "../assets/Tomato.png";
 
-export default function Customize() {
+export default function Customize({ ingredients, setIngredients }) {
+  
+  const changeIngredients = (name) => {
+    let newIngredients = JSON.parse(JSON.stringify(ingredients)); 
+    // Toggle That Ingredient  25:16
+    newIngredients[name] = !newIngredients[name]
+    setIngredients(newIngredients);
+  }
+  
   return (
     <div style= {{ display: "flex"}}>
-      <div style={{ border: "2px solid black", flex: 1 }}>Image</div>
-      <div style={{ border: "2px solid black", flex: 1 }}>Checkbox</div>
+      {JSON.stringify(ingredients)}
+      <div style={{ border: "2px solid black", flex: 1 }}>Image
+        <div style={{maxHeight:500, maxWidth: 500, position: "relative" }}>
+          <img src={Cheese} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Olive} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Pineapple} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Mushroom} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Basil} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Tomato} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
+          <img src={Base} alt="Pizza Base" height="100%" width= "100%" />
+        </div>
+      </div>
+      <div style={{ border: "2px solid black", flex: 1 }}>
+        <input type="checkbox" id="pineaple" name="pineaple" value="Bike" onChange={() => changeIngredients("pineaple")}/>
+        <label for="pineaple">Pineapple</label>
+      </div>
     </div>
   )
 }
