edc3199e2eae033c37e13248cb9a4f0d37d9e0e0
Bulcao <ambulcao@gmail.com>
2020-09-21 21:54:59 +0100

    Adjusting Customize.js and Index.css

--- src/components/Customize.js
+++ src/components/Customize.js
@@ -8,18 +8,23 @@ import Basil from "../assets/Basil.png";
 import Tomato from "../assets/Tomato.png";
 
 export default function Customize({ ingredients, setIngredients }) {
-  
-  const changeIngredients = (name) => {
-    let newIngredients = JSON.parse(JSON.stringify(ingredients)); 
-    // Toggle That Ingredient  25:16
-    newIngredients[name] = !newIngredients[name]
+  const onChange = (event, name) => {
+    //console.log(localStorage);
+    let newIngredients = JSON.parse(JSON.stringify(ingredients));
+    newIngredients[name] = event;
     setIngredients(newIngredients);
-  }
+    localStorage.setItem("ingredients", JSON.stringify(newIngredients));
+  };
+  //const changeIngredients = (name) => {
+  //  let newIngredients = JSON.parse(JSON.stringify(ingredients)); 
+    // Toggle That Ingredient  25:16 
+  //  newIngredients[name] = !newIngredients[name]
+  //  setIngredients(newIngredients);
+  
   
   return (
     <div style= {{ display: "flex"}}>
-      {JSON.stringify(ingredients)}
-      <div style={{ border: "2px solid black", flex: 1 }}>Image
+       <div style={{ border: "2px solid black", flex: 1 }}>Image
         <div style={{maxHeight:500, maxWidth: 500, position: "relative" }}>
           <img src={Cheese} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
           <img src={Olive} alt="Pizza Base" height="100%" width= "100%" className="ingredients" />
@@ -31,9 +36,73 @@ export default function Customize({ ingredients, setIngredients }) {
         </div>
       </div>
       <div style={{ border: "2px solid black", flex: 1 }}>
-        <input type="checkbox" id="pineaple" name="pineaple" value="Bike" onChange={() => changeIngredients("pineaple")}/>
-        <label for="pineaple">Pineapple</label>
+        <label class="container-checkbox">
+          Pineapple
+          <input
+            type="checkbox"
+            checked={ingredients["pineapple"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "pineapple")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">
+          Basil
+          <input
+            type="checkbox"
+            checked={ingredients["basil"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "basil")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">
+          Olive
+          <input
+            type="checkbox"
+            checked={ingredients["olive"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "olive")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">
+          Tomato
+          <input
+            type="checkbox"
+            checked={ingredients["tomato"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "tomato")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">
+          Mushroom
+          <input
+            type="checkbox"
+            checked={ingredients["mushroom"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "mushroom")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">
+          Cheese
+          <input
+            type="checkbox"
+            checked={ingredients["cheese"]}
+            onChange={(event) => 
+              onChange(event.currentTarget.checked, "cheese")
+            } 
+          />
+        <span class="checkmark"></span>
+        </label>
       </div>
     </div>
-  )
+  );
 }
